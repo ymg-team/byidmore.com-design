5a9b309b2b63417fb6efca9cf6cae00644a1bce4
yussan <yussandevelper@gmail.com>
2017-09-21 00:01:46 +0700

    added academy detail

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,9 +49,30 @@ module.exports = grunt => {
             watchTask: true,
             server: './dist'
         }
-    }
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          // includes files within path
+          // {expand: true, src: ['path/*'], dest: 'dest/', filter: 'isFile'},
+    
+          // includes files within path and its sub-directories
+          {expand: true, cwd:'src/', src: ['images/**', 'fonts/**'], dest: 'dist'},
+    
+          // makes all src relative to cwd
+          // {expand: true, cwd: 'path/', src: ['**'], dest: 'dest/'},
+    
+          // flattens results to a single level
+          // {expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'},
+        ],
+      },
     },
     watch: {
+      copy: {
+        files: ['src/images/**/*'],
+        tasks: ['copy']
+      },
       pug: {
         files: ['src/pug/**/*.pug'],
         tasks: ['pug']
@@ -73,15 +94,16 @@ module.exports = grunt => {
   grunt.loadNpmTasks('grunt-contrib-uglify')
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   
   let tasks = []
   //register default task
   if(process.env.NODE_ENV != 'production')
   {
-    tasks = ['pug', 'sass', 'uglify', 'browserSync', 'watch'];
+    tasks = ['pug', 'sass', 'uglify', 'copy', 'browserSync', 'watch'];
   }else
   {
-    tasks = ['pug', 'sass', 'uglify'];
+    tasks = ['pug', 'sass', 'uglify', 'copy'];
   }
 
   grunt.registerTask('default', tasks)
